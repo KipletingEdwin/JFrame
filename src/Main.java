cc0f8feb3216fde426d159be51b618b485b074ba
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-11-26 12:17:27 +0000

    setDefaultCloseOperation to Exit on close

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         // JFrame  =  A GUI window to add components to
         JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.setSize(420,420);
 
