c745a7b8f3fcf075ae275812ad1cd0a02143e591
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-11-28 16:22:46 +0000

    Add a new class

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,21 @@ import java.awt.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // JFrame  =  A GUI window to add components to
 
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Title goes here");
-        frame.setSize(420,420);
-        frame.setResizable(false);
 
-        frame.setVisible(true);
-        frame.getContentPane().setBackground(new Color(255,0,0)); //change color of background
+
+//        // JFrame  =  A GUI window to add components to
+//
+//        JFrame frame = new JFrame();
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setTitle("Title goes here");
+//        frame.setSize(420,420);
+//        frame.setResizable(false);
+//
+//        frame.setVisible(true);
+//        frame.getContentPane().setBackground(new Color(255,0,0)); //change color of background
+
+
 
     }
 }
