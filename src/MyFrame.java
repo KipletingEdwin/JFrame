05c1370c39e3a83a3c6d48d43885391a3dc79919
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-11-28 16:26:58 +0000

    Add contents to JFrame

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,8 +1,20 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MyFrame extends JFrame {
 
     MyFrame(){
+
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Title goes here");
+        this.setSize(420,420);
+        this.setResizable(false);
+
+        this.setVisible(true);
+        this.getContentPane().setBackground(new Color(255,0,0)); //change color of background
+
+
 
     }
 }
